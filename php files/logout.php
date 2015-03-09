d8eee062b8b979466f26297ab17c4dbcc142c14c
jet2411 <izzatul.samsudin@gmail.com>
2015-03-09 11:30:34 +0000

    amend

--- php files/logout.php
+++ php files/logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
 session_destroy();
-header('Location: index.php');
+header('Location: index.php'); // need to redirect to login page. index.php right now is the homepage.
 ?>
