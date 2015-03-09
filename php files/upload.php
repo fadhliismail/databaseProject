935d5a9c645e2b4f57916bdffe4606b5ca3748b0
jet2411 <izzatul.samsudin@gmail.com>
2015-03-09 20:11:03 +0000

    edited upload.php

--- php files/upload.php
+++ php files/upload.php
@@ -8,9 +8,9 @@ include 'db_connect.php';
 
 $GroupNo = 4;
 
-$query = "INSERT INTO report (GroupNo, File_Name, File_Size, File_Link, File_Type, Submission_Timestamp) VALUES (?, ?, ?, ?, ?, ?)";
-$queryReport = "LOAD XML LOCAL INFILE '?' INTO TABLE report ROWS IDENTIFIED BY '<report>'";
-//http://localhost/virtual_learning/uploads/report.xml
+
+$query = "LOAD XML LOCAL INFILE '?' INTO TABLE report ROWS IDENTIFIED BY '<report>' SET GroupNo = ?, File_Name = ?, File_Size = ?, File_Link = ?, File_Type = ?, Submission_Timestamp = ?";
+
 $ds = "/";
 $storeFolder = 'uploads';
 
@@ -21,6 +21,7 @@ if(!empty($_FILES)) {
 	$myfileSize = $_FILES['file']['size'];
 	$myfileLink = 'http://localhost/virtual_learning/uploads/' . rawurlencode($myfileName);
 	$myfileDate = date("Y-m-d H:i:s");
+	echo $myfileName;
 
 	$tempFile = $_FILES['file']['tmp_name'];
 	$targetPath = dirname(__FILE__) . $ds . $storeFolder . $ds;
@@ -28,24 +29,18 @@ if(!empty($_FILES)) {
 	move_uploaded_file($tempFile, $targetFile);
 
 	$stmt = $conn->prepare($query);
-	$stmt->bind_param('isisss', $GroupNo, $myfileName, $myfileSize, $myfileLink, $myfileType, $myfileDate);
-
+	$stmt->bind_param('sisisss', $myfileName, $GroupNo, $myfileName, $myfileSize, $myfileLink, $myfileType, $myfileDate);
+	
 	if($stmt->execute()) {
 		echo 'Success!';
 	} else {
 		die('Error: ('. $conn->errno . ') ' . $conn->error);
 	}
 
-	$stmtReport = $conn->prepare($queryReport);
-	$stmtReport->bind_param('s', $myfileLink);
-	$stmtReport->execute();
 }
 
 //Close statement
 $stmt->close();
-
-//Close 2nd statement
-$stmtReport->close();
 
 //Close connection
 $conn->close();
