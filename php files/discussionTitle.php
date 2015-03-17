5317af0f44284c6578904fbb8832669865ae1b4b
yusuke1116 <yusuke.kawano@gmail.com>
2015-03-17 17:29:19 +0000

    discussion board revise

--- php files/discussionTitle.php
+++ php files/discussionTitle.php
@@ -25,17 +25,15 @@ function exception_to_array(Exception $e) {
 }
 
 //initialize variables
-foreach (array('title','name', 'text','assessmentID', 'token', 'page', 'submit') as $v) {
+foreach (array('id','title','name', 'text','assessmentID', 'page', 'submit') as $v) {
     $$v = isset($_POST[$v]) && is_string($_POST[$v]) ? trim($_POST[$v]) : '';
 }
-// page number must be over 1 page
-$page = max(1, (int)$page);
-
 //initialize session
 session_name(SESSION_NAME); 
 @session_start();           
 if (!$_SESSION) {
     $_SESSION = array(
+        'id'    => '',
         'title' => '',
         'name'  => '',
         'text'  => '',
@@ -50,67 +48,29 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // code of pushing submission button
     if ($submit) {
-        //$id is AssessmentID
+        //$id is assessmentNo
         $id = $_GET['id'];
-           
         try {
             $_SESSION['name'] = $name;
             $_SESSION['text'] = $text;
             $_SESSION['title'] = $title;
             $_SESSION['assessmentID'] = $id;
-       
-            if (!isset($_SESSION['token'][$token])) {
-                throw e('The expiration date is outdated', $e);
-            }
-            unset($_SESSION['token'][$token]);
-            
-            // if the submission interval is under request time, show this message
-            if ($_SESSION['prev'] !== null) {
-                $diff = $_SERVER['REQUEST_TIME'] - $_SESSION['prev'];
-                if (($limit = LIMIT_SEC - $diff) > 0) {
-                    throw e("Your submission interval is too short to write. Please wait {$limit} second", $e);
-                }
-            }
-            // name check
-            if (!$len = mb_strlen($name) or $len > 30) {
-                $e = e('put your name under 30 character length', $e);
-            }
-            // text check
-            if (!$len = mb_strlen($text) or $len > 140) {
-                $e = e('put your text under 140 character length', $e);
-            }
+                 
             if (!empty($e)) {
                 throw $e;
-            }
-            $stmt = $pdo->prepare(implode(' ', array(
-                'INSERT',
-                'INTO mini_board(`title`,`name`, `text`,`assessmentID`,`time`)',
-                'VALUES( ?,?, ?, ?, ?)',
-            )));
-            //execute writing 
-            $stmt->execute(array(
-                $title,
-                $name,
-                $text,
-                $id,
-                date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME'])
-            ));
-            // set time session information
-            $_SESSION['prev'] = $_SERVER['REQUEST_TIME'];
-            $_SESSION['text'] = '';
+            }      
             // when the submission finished, the message will be shown
             throw e('submission finished', $e);
         } catch (Exception $e) { }
     }
     $id = $_GET['id'];
+
     $stmt = $pdo->prepare(implode(' ', array(
         'SELECT',
-        'SQL_CALC_FOUND_ROWS `name`, `text`, `time`,`AssessmentID`,`title`',
+        'SQL_CALC_FOUND_ROWS `id`,`name`,`text`, `time`,`AssessmentID`, `title`',
         'FROM mini_board',
         'WHERE AssessmentID =',
         $id,
-        'ORDER BY `id` DESC',
-        'LIMIT ?, ?',
     )));
     $stmt->bindValue(1, ($page - 1) * DISP_MAX, PDO::PARAM_INT);
     $stmt->bindValue(2, DISP_MAX, PDO::PARAM_INT);
@@ -121,12 +81,6 @@ try {
     $page_count = ceil($whole_count / DISP_MAX);
 
 } catch (Exception $e) { }
-
-$_SESSION['token'] = array_slice(
-    array($token = sha1(mt_rand()) => 1) + $_SESSION['token'],
-    0,
-    TOKEN_MAX
-);
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -164,7 +118,6 @@ $_SESSION['token'] = array_slice(
 					<li role="presentation"><a href="review.php">Review</a></li>
 					<li role="presentation" class="active"><a href="discussion.php">Discussion</a></li>
 					<li role="presentation"><a href="help.php">Help</a></li>
-
 				</ul>
 				<ul class="nav navbar-nav navbar-right">
 					<li><a href="registerPage.php">Register</a></li>
@@ -181,14 +134,7 @@ $_SESSION['token'] = array_slice(
         echo '<h1>discussion board#'.$id.'</h1>' ?>
       </div>
       <div>
-        <div id="textarea">
-          <form action="" method="post">
-              <label>name: <input name="name" type="text" value="<?=h($_SESSION['name'])?>" /></label><br>
-                  <label>text<p><textarea name="text" rows="4"cols="40"><?=h($_SESSION['text'])?></textarea></p></label><br>
-            <label style="text-align:left;"><input type="submit" name="submit" value="submission" /></label>
-            <label><input type="hidden" name="token" value="<?=h($token)?>" /></label>
-          </form>
-        </div>
+      
 <?php if (!empty($e)): ?>
         <div id="messages">
 <?php foreach (exception_to_array($e) as $msg): ?>
@@ -197,41 +143,31 @@ $_SESSION['token'] = array_slice(
         </div>
 <?php endif; ?>
 <?php if (!empty($articles)): ?>
-        <div id="articles">
 <?php foreach ($articles as $article): ?>
-          <div class="article">
-            <div class="article_text"><pre><?=h($article['text'])?></pre></div>
-            <div class="article_time"><?=h($article['time'])?></div>
-          </div>
+           <div class="table-responsive">
+		<table class ="table table-nonfluid">
+                        <tr>
+				<th>Discussion Title</th>
+				<th>started by</th>
+				<th>last post</th>
+			</tr>
+                        <tr>
+                                
+                                <?php  $Address=h($article['AssessmentID']); 
+                                       $TitleName=h($article['title']); 
+                                echo '<td><a href="discussion.php?id='.$Address.'">'.$TitleName.'</a></td>';
+                                ?>                              
+                                <td><?=h($article['name'])?> </td>                                                              
+                                <td></td>
+                        </tr>  
+            	</table>
+            </div>
 <?php endforeach; ?>
         </div>
 <?php endif; ?>
-      </div>
-      <div >
-        <div>
-<?php if ($page > 1): ?>
-          <a href="?page=<?=$page-1?>">before</a> | 
-<?php endif; ?>
-          <a href="?">latest</a>
-<?php if (!empty($page_count) and $page < $page_count): ?>
-           | <a href="?page=<?=$page+1?>">next</a>
-<?php endif; ?>
-        </div>
-        <p class="page"><?php
-          if (empty($current_count)) {
-            echo 'There are no comment';
-          } else {
-            printf('%d comment of %d to %d comment(%dpage of %dpage)',
-              $whole_count,
-              ($tmp = ($page - 1) * DISP_MAX) + 1,
-              $tmp + $current_count,
-              $page_count,
-              $page
-            );
-          }
-        ?></p>
       </div>
     </div>
   </body>
     <?php include 'footer.php'?>
 </html>
+
